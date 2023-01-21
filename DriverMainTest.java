b620cc52ceec71671719672a9ac35bc53af3cc1c
csdomokos <95182525+csdomokos@users.noreply.github.com>
2023-01-21 13:46:14 +0100

    Gmail open test

---
+++ DriverMainTest.java
@@ -0,0 +1,37 @@
+package hu.testExample.driverfactory;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class DriverMainTest {
+    protected WebDriver driver;
+
+    @BeforeAll
+    public void driverSetup() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+
+        //Creating an object of ChromeDriver
+        driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(30000));
+        driver.manage().window().maximize();
+        //Deleting all the cookies
+        driver.manage().deleteAllCookies();
+
+        //Specifiying pageLoadTimeout and Implicit wait
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofMillis(30000));
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(30000));
+    }
+
+
+    @AfterAll
+    public void driverCleanUp() {
+        driver.quit();
+    }
+}
---
+++ GmailHomeTest.java
@@ -0,0 +1,14 @@
+package hu.testExample.testExample;
+
+import hu.testExample.driverfactory.DriverMainTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class GmailHomeTest extends DriverMainTest {
+
+    @Test
+    public void openGoogleHomeScreenTest() {
+        driver.get("https://gmail.com");
+    }
+}
